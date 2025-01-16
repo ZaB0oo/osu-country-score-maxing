a689b2f481cf29625c4a945db73d55127b3c6259
Axel SULMONT <axel.sulmont@viacesi.fr>
2025-01-16 22:52:52 +0100

    fix

--- french_missing_score.py
+++ french_missing_score.py
@@ -23,7 +23,6 @@ def main():
         print(f"Erreur lors de la connexion à l'API : {e}")
         return
 
-    total_diff_of_mapset_filtered, total_diff_filtered, total_added, total_difference = 0, 0, 0, 0
     try:
         if os.path.exists(BEATMAPS_FILE):
             os.remove(BEATMAPS_FILE)
@@ -33,59 +32,54 @@ def main():
         print(f"Erreur lors de la manipulation du fichier : {e}")
         return
     
-    start_index = loadProgress()
+    start_index, total, total_difference = loadProgress()
     beatmaps = loadData()
 
-    for beatmap in beatmaps:
-        total_diff_of_mapset_filtered += 1
-        try:
-            if beatmap["stars"] < 8 and beatmap["circles"] + beatmap["sliders"] + beatmap["spinners"] > 400:
-                total_diff_filtered += 1
+    for i, beatmap in enumerate(beatmaps[start_index:], start=start_index + 1):
+        if beatmap["stars"] < 8 and beatmap["circles"] + beatmap["sliders"] + beatmap["spinners"] > 400:
+            try:
+                time.sleep(1)
+                scores = api.beatmap_scores(beatmap["beatmap_id"], mode="osu", limit=100)
+            except Exception as e:
+                print(f"Erreur lors de la récupération des scores de la beatmap {beatmap['beatmap_id']} : {e}")
+                continue
+
+            if not scores.scores:
+                continue
+
+            best_global_score = scores.scores[0]
+            if best_global_score._user.country_code == "FR":
+                continue
+
+            french_scores = [score for score in scores.scores if score._user.country_code == "FR"]
+
+            if not french_scores:
                 try:
-                    time.sleep(1)
-                    scores = api.beatmap_scores(beatmap["beatmap_id"], mode="osu", limit=100)
-                except Exception as e:
-                    print(f"Erreur lors de la récupération des scores de la beatmap {beatmap['beatmap_id']} : {e}")
-                    continue
-
-                if not scores.scores:
-                    continue
-
-                best_global_score = scores.scores[0]
-                if best_global_score._user.country_code == "FR":
-                    continue
-
-                french_scores = [score for score in scores.scores if score._user.country_code == "FR"]
-
-                if not french_scores:
-                    try:
-                        valid, difference = compare_scores(best_global_score.classic_total_score, scores.scores[-1].classic_total_score, PERCENTAGE_LIMIT)
-                        if valid:
-                            total_added += 1
-                            total_difference += difference
-                            add_beatmap(beatmap, difference, total_added)
-                    except Exception as e:
-                        print(f"Erreur lors de la comparaison des scores de la beatmap {beatmap['beatmap_id']} : {e}")
-                    continue
-
-                best_french_score = max(french_scores, key=lambda x: x.classic_total_score)
-                try:
-                    valid, difference = compare_scores(best_global_score.classic_total_score, best_french_score.classic_total_score, PERCENTAGE_LIMIT)
+                    valid, difference = compare_scores(best_global_score.classic_total_score, scores.scores[-1].classic_total_score, PERCENTAGE_LIMIT)
                     if valid:
-                        total_added += 1
+                        total += 1
                         total_difference += difference
-                        add_beatmap(beatmap, difference, total_added)
+                        add_beatmap(beatmap, difference, total)
+                        save_progress(i, total, total_difference)
                 except Exception as e:
                     print(f"Erreur lors de la comparaison des scores de la beatmap {beatmap['beatmap_id']} : {e}")
-        except Exception as e:
-            print(f"Erreur lors de l'analyse de la beatmap {beatmap['beatmap_id']} : {e}")
+                continue
+
+            best_french_score = max(french_scores, key=lambda x: x.classic_total_score)
+            try:
+                valid, difference = compare_scores(best_global_score.classic_total_score, best_french_score.classic_total_score, PERCENTAGE_LIMIT)
+                if valid:
+                    total += 1
+                    total_difference += difference
+                    add_beatmap(beatmap, difference, total)
+                    save_progress(i, total, total_difference)
+            except Exception as e:
+                print(f"Erreur lors de la comparaison des scores de la beatmap {beatmap['beatmap_id']} : {e}")
 
     os.remove(PROGRESS_FILE)
     try:
-        with open(f"results.txt", "a") as f:
-            f.write(f"Total des beatmaps analysées : {total_diff_of_mapset_filtered}\n")
-            f.write(f"Total des beatmaps comparées : {total_diff_filtered}\n")
-            f.write(f"Total des beatmaps récupérées : {total_added}\n")
+        with open(BEATMAPS_FILE, "a") as f:
+            f.write(f"Total des beatmaps récupérées : {total}\n")
             f.write(f"Total de score manquant : {'{:,}'.format(total_difference)}\n")
     except Exception as e:
         print(f"Erreur lors de l'écriture du fichier récapitulatif : {e}")
@@ -95,8 +89,19 @@ def loadProgress():
         with open(PROGRESS_FILE, "r") as f:
             lines = f.readlines()
             start_index = int(lines[0].strip())
-            return start_index
+            total = int(lines[1].strip())
+            total_difference = int(lines[2].strip())
+            return start_index, total, total_difference
     return 0
+
+def save_progress(index, total, total_difference):
+    try:
+        with open(PROGRESS_FILE, "w") as f:
+            f.write(f"{index}\n")
+            f.write(f"{total}\n")
+            f.write(f"{total_difference}\n")
+    except Exception as e:
+        print(f"Erreur lors de la sauvegarde de la progression : {e}")
 
 def loadData(file_path="beatmaps.csv"):
     beatmaps = []
@@ -134,11 +139,11 @@ def compare_scores(global_score, other_score, limit):
         print(f"Erreur inattendue lors de la comparaison des scores : {e}")
         return False, 0
 
-def add_beatmap(beatmap, difference, total_added):
+def add_beatmap(beatmap, difference, total):
     try:
-        with open(f"beatmaps.txt", "a") as f:
+        with open(BEATMAPS_FILE, "a") as f:
             f.write(f"{beatmap['beatmap_id']};{beatmap['title']} {beatmap['diffname']};{beatmap['ranked_date']};{beatmap['stars']}*;{beatmap['bpm']} BPM;{beatmap['ar']};{beatmap['od']};{beatmap['cs']};{beatmap['hp']};{beatmap['length']};{'{:,}'.format(difference)}\n")
-        print(f"Beatmap n°{total_added} ajoutée : {beatmap['title']} - {beatmap['beatmap_id']}")
+        print(f"Beatmap n°{total} ajoutée : {beatmap['title']} - {beatmap['beatmap_id']}")
     except Exception as e:
         print(f"Erreur lors de l'écriture de la beatmap {beatmap['beatmap_id']} dans le fichier : {e}")
 
